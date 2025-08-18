f9d5fad026c15ff146982b175ab658dc25b84f89
Finn4239 <finn.oettinger@outlook.de>
2025-08-18 19:43:56 +0200

    added Footer Template and implemented for AboutUsPage and ContactPage

---
+++ Models/FooterContentViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LuxuryAndCo.OnlineShop.Models;
+
+public class FooterContentViewModel
+{
+    public string PageTitle
+    { get; set; }
+    
+    public List<Section> Sections { get; set; } = new List<Section>();
+    
+}
+
+public class Section
+{
+    public string Title { get; set; }   // h2
+    public string Content { get; set; }
+}
