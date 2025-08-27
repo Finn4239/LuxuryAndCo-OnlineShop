f5df66f1e490df60b8a4b966fbaadfe013286e72
Finn4239 <finn.oettinger@outlook.de>
2025-08-27 03:01:58 +0200

    implemented all Shopping Pages and small enhancement of ProductCardViewModel.cs

--- Models/ProductCardViewModel.cs
+++ Models/ProductCardViewModel.cs
@@ -2,7 +2,8 @@
 
 public class ProductCardViewModel
 {
-    public string ImageUrl { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
 }
