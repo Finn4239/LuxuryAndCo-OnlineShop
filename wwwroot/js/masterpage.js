55205b6213e068f279384ac667d078ec0aa56f27
Finn4239 <finn.oettinger@outlook.de>
2025-08-29 05:39:30 +0200

    refactor page templates to enhance layout and implement dynamic content for multilingual support

---
+++ wwwroot/js/masterpage.js
@@ -0,0 +1,7 @@
+﻿const onToggleLanguage = (currentLang) => {
+    window.location.href = currentLang === 'en' ? '/de' : '/';
+    
+    const toggleFlagButton = document.getElementById('flag-toggle');
+    toggleFlagButton.setAttribute('data-language', currentLang);
+    languageFlag.className = currentLang === 'en' ? "fi fi-gb" : "fi fi-de";
+}
