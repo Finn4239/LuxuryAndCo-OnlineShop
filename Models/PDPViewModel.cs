29e3f6878fc077143e416cd5f56c49a1fc6d89e4
Finn4239 <finn.oettinger@outlook.de>
2025-08-29 16:25:06 +0200

    add ProductDetailPage and implement PDPView for product details display

---
+++ Models/PDPViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LuxuryAndCo.OnlineShop.Models;
+
+public class PDPViewModel
+{
+    public string Brand { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public List<string> ImageUrls { get; set; } = new List<string>();
+    
+}
